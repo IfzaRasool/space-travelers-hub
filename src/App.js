1105ac5550a7655b4ba087a053af0da46dfa9f1a
Damola Adediran <tylher123@gmail.com>
2022-05-09 15:02:23 +0100

    create router paths

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/header";
+import Mission from "./components/Mission";
+import MyProfile from "./components/MyProfile";
+import Rockets from "./components/Rockets";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Rockets/>}></Route>
+          <Route path='/mission' element={<Mission/>}></Route>
+          <Route path='/myprofile' element={<MyProfile/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export const header = () => {
+export const Header = () => {
+  const navigation = [
+    { id: 1, link: '/',name:'rockets' },
+    { id: 2, link: '/mission',name:'mission' },
+    { id: 3, link: '/myprofile',name:'profile' },  
+  ]
   return (
     <div>
         <header>
