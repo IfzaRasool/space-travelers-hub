e7350d990d6e12d6ab1f469797ede66f23d49072
IfzaRasool <ifzarasoolarain@gmail.com>
2022-05-09 16:42:10 +0300

    Add header file

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const header = () => {
+  return (
+    <div>
+        <header>
+        <nav>
+            
+        </nav>
+
+        </header>
+    </div>
+  )
+}
