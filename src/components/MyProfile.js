c7fde5031751a10e5663fd800434fc9a24ff9b72
IfzaRasool <ifzarasoolarain@gmail.com>
2022-05-11 19:04:02 +0300

    page layout

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './MyProfile.css';
 
 const MyProfile = () => (
   <div>
-    <div>
-      <h3>My Missions</h3>
-
+    <div className="myProfile-container">
+      <div className="mission-item">
+        <h3> My Missions</h3>
+        <h6>dfds</h6>
+      </div>
+      <div>
+        <h3>My Rockets</h3>
+      </div>
     </div>
-    <div> My Rockets</div>
   </div>
 );
 
